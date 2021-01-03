6b96f7d4bfb0175c6a30470f0b7ad8541f506d45
Sam Warren <samwarren4@gmail.com>
2021-01-03 15:16:29 -0800

    initial commit

---
+++ webclient/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Landing />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Landing from "@/components/Landing.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    Landing
+  }
+})
+export default class Home extends Vue {}
+</script>
