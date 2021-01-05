8126ef4764ced18fc65c938debeefae65fb36643
Sam Warren <samwarren4@gmail.com>
2021-01-04 16:58:35 -0800

    add loading component

--- client/src/views/LoginCallback.vue
+++ client/src/views/LoginCallback.vue
@@ -1,8 +1,9 @@
 <template>
   <v-container>
-    <v-row class="text-center mt-4">
+    <LoadingComponent :is-loading="isLoading"></LoadingComponent>
+    <v-row v-if="!isLoading" class="text-center mt-4">
       <v-col></v-col>
-      <v-col cols="4">
+      <v-col v-if="gotBearer" cols="4">
         <v-img
           :src="require('@/assets/images/material-checkmark.svg')"
           class="my-3"
@@ -10,9 +11,21 @@
           height="100"
         />
         <h1 class="pt-4 display-1 font-weight-medium">
-          Nice! You're now linked to Twitch. Head back to the homepage to
-          continue setting up!
+          Nice!
         </h1>
+        <h2 class="pt-4 display-0 font-weight-light">
+          You're now linked to Twitch. Head back to the homepage to continue
+          setting up
+        </h2>
+      </v-col>
+      <v-col v-else cols="4">
+        <h1 class="pt-4 display-1 font-weight-medium">
+          Uh oh...
+        </h1>
+        <h2 class="pt-4 display-0 font-weight-light">
+          Something went wrong trying to connect you to Twitch. Please head back
+          to the homepage and click "Authenticate" again.
+        </h2>
       </v-col>
       <v-col></v-col>
     </v-row>
@@ -23,19 +36,27 @@
 import axios from "axios";
 import { Component, Vue } from "vue-property-decorator";
 import VueRouter from "vue-router";
-import router, { Route } from "vue-router";
+import LoadingComponent from "../components/Loading.vue";
 
-@Component({})
+@Component({
+  components: {
+    LoadingComponent: LoadingComponent
+  }
+})
 export default class LoginCallback extends Vue {
-  router = new VueRouter({});
+  private router = new VueRouter({});
+  private gotBearer = false;
+  private isLoading = true;
 
   private mounted() {
     console.log(this.$route.query.code);
     axios({
-      url: "http://localhost:5000/get-token?code=" + this.$route.query.code,
+      url: "http://localhost:5010/get-token?code=" + this.$route.query.code,
       method: "GET"
     }).then(res => {
+      res.status === 200 ? (this.gotBearer = true) : (this.gotBearer = false);
       console.log("Result of bearer: ", res);
+      this.isLoading = false;
     });
   }
 }
